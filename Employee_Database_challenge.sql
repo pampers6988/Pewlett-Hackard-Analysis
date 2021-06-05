d2109a281337ef79ad97baa840c1d6b8b8140f02
pampers6988 <74462990+pampers6988@users.noreply.github.com>
2021-06-05 18:54:50 -0500

    Update Employee_Database_challenge.sql

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+#retirement titles
 select
 e.emp_no, 
 e.first_name, 
@@ -12,3 +13,16 @@ on (e.emp_no=ti.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 order by e.emp_no ;
 
+#Unique titles
+select distinct on(e.emp_no)
+e.emp_no,
+e.first_name,
+e.last_name,
+ti.title,
+ti.from_date,
+ti.to_date,
+e.birth_date
+INTO unique_titles
+from employees as e
+JOIN titles as ti
+on (e.emp_no=ti.emp_no)
