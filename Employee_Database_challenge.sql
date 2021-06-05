2f3844a4092ba7a90b5d83e02c54ec8f3c8eeba3
pampers6988 <74462990+pampers6988@users.noreply.github.com>
2021-06-05 18:48:39 -0500

    Update Employee_Database_challenge.sql

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,1 +1,14 @@
+select
+e.emp_no, 
+e.first_name, 
+e.last_name,
+ti.title,
+ti.from_date,
+ti.to_date
+INTO retirement_titles
+from employees as e
+join titles as ti
+on (e.emp_no=ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+order by e.emp_no ;
 
