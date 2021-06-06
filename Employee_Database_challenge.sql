3c05d7d9a172c34c20936a8cb43d8c8f7fa1b493
pampers6988 <74462990+pampers6988@users.noreply.github.com>
2021-06-06 11:55:19 -0500

    Update Employee_Database_challenge.sql

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -66,4 +66,22 @@ from retiring_titles
 group by title
 order by "Title Counts" desc;
 
+--mentorship eligibility
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ti.title
+INTO mentorship_eligibility
+FROM employees as e
+inner JOIN dept_emp  de
+ON e.emp_no = de.emp_no
+inner join titles ti
+ON e.emp_no = ti.emp_no
+WHERE de.to_date = ('9999-01-01')
+order by e.emp_no
+
+
 
